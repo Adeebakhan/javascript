3268c771420aa79482fef16e38fc1974cdd79d2f
Adeeba khan <106258367+Adeebakhan@users.noreply.github.com>
2023-10-15 10:04:22 +0000

    conversions of datatypes

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,20 @@
+// let score="33abc"
+// console.log(score);
+// console.log(typeof score)
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+//"33"=>33
+//"33abc"=>NaN
+//true=>1;false=>0
+
+// let isLoggedIn=1
+
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+//1=>true;0=>false
+//""=>false; "adeeba"=>true
+let number=33
+let stringNumber=String(number)
+console.log(stringNumber);
+console.log(typeof stringNumber);
