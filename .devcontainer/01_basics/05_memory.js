7390e6489a6b3f6596a3e387bba946d9cb4c3afb
Adeeba khan <106258367+Adeebakhan@users.noreply.github.com>
2023-10-16 15:16:59 +0000

    memory

---
+++ .devcontainer/01_basics/05_memory.js
@@ -0,0 +1,14 @@
+let score=100
+let newScore=score
+newScore=114
+console.log(score);
+console.log(newScore);//primitive datatypes
+let order1={
+    name:"biryani",
+    qunatity:"full",
+    amount:150,
+}
+let order2=order1
+order2.name="Chicken curry"
+console.log(order1);
+console.log(order2);
