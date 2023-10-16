96e4899df74e33b9d87c30edebf6329b2f4241a9
Adeeba khan <106258367+Adeebakhan@users.noreply.github.com>
2023-10-16 17:05:19 +0000

    symbols

--- .devcontainer/01_basics/04_symbols.js
+++ .devcontainer/01_basics/04_symbols.js
@@ -1,16 +1,16 @@
-// const id=Symbol(146)
-// const newId=Symbol(146)
-// console.log(id===newId);
+const id=Symbol(146)
+const newId=Symbol(146)
+console.log(id===newId);
 // arrays
 const planets=(["earrth","venus","mercury"]);
 //objects
-// const myObj={
-//     name:"Adeeba khan",
-//     age:20,
-// }
-// //functions
-// const myFunction=function(){
-//     console.log("Hello world")
-// }
-// console.log(typeof planets)
+const myObj={
+    name:"Adeeba khan",
+    age:20,
+}
+//functions
+const myFunction=function(){
+    console.log("Hello world")
+}
+console.log(typeof planets)
 console.log(Array.isArray(planets))
